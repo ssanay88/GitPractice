99a766eef640d6f0555edc08e91fa1db1799841e
ssanay88 <pplloo748@gmail.com>
2021-04-29 21:23:40 +0900

    Hello world 대사를 수정

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.gitpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-
+// 내가 변경할 코드
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Hello World!!!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "와라 세상아!!!", Toast.LENGTH_SHORT).show()
     }
 }
